ff4dfd99ba64d9067a98324c89d310985a11472a
thrx.de <coder@thrx.de>
2021-06-24 00:45:18 +0200

    remove display glitches

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -35,7 +35,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "1.1.9a"
+  #define SHORT_BUILD_VERSION "1.1.9.1b"
 
   /**
    * Verbose version identifier which should contain a reference to the location
