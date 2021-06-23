cbf62e17fc24bc48ec1ddb0981b9a6444b75402e
thrx.de <coder@thrx.de>
2021-06-23 21:31:31 +0200

    new version

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -48,7 +48,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2020-03-28"
+  #define STRING_DISTRIBUTION_DATE "2021-06-23"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
