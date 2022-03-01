62b2cbe37b2b27744614c2036fb7fef56e119270
thrx.de <coder@thrx.de>
2022-03-01 22:39:18 +0100

    update config for cr-10 v2

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,32 +28,25 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "2.1.2.5"
+#define SHORT_BUILD_VERSION "2.1.2"
 
 /**
  * Verbose version identifier which should contain a reference to the location
  * from where the binary was downloaded or the source code was compiled.
  */
-//#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2024-11-18"
-
-/**
- * The protocol for communication to the host. Protocol indicates communication
- * standards such as the use of ASCII, "echo:" and "error:" line prefixes, etc.
- * (Other behaviors are given by the firmware version and capabilities report.)
- */
-//#define PROTOCOL_VERSION "1.0"
+#define STRING_DISTRIBUTION_DATE "2023-01-06"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-//#define MACHINE_NAME "3D Printer"
+#define MACHINE_NAME "thrx CR-10 V2"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
@@ -61,7 +54,7 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-//#define SOURCE_CODE_URL "github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "github.com/thrxorg/thrx-marlin-firmware/tree/thrx/creality-cr-10-v2/2.1.x"
 
 /**
  * Default generic printer UUID.
